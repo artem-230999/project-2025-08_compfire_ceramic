020a94a311c23fa8db5227c50b26560f3079c65a
Artem2309_web <78473307+Artem23-09@users.noreply.github.com>
2025-08-20 10:33:25 +0200

    add forms validate

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,13 @@
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
+import JustValidate from 'just-validate';
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "/src/sass/style.scss";
 
+// Burger
 const burger = document.querySelector('.burger');
 const close = document.querySelector('.header__menu-close');
 const menu = document.querySelector('.header__menu');
@@ -19,10 +21,11 @@ close.addEventListener('click', () => {
   menu.classList.remove('header__menu--active');
   document.body.style.overflow = '';
 });
+// Burger
 
-
+// Swiper
 try {
-	new Swiper(".works__slider", {
+  new Swiper(".works__slider", {
 		slidesPerView: 1,
 		loop: true,
 		pagination: {
@@ -30,7 +33,7 @@ try {
 			clickable: true,
 		},
 		navigation: {
-			nextEl: ".icon-right-open",
+      nextEl: ".icon-right-open",
 			prevEl: ".icon-left-open",
 		},
 		breakpoints: {
@@ -39,17 +42,18 @@ try {
 				spaceBetween: 5,
 			},
 			1920: {
-				spaceBetween: 35,
+        spaceBetween: 35,
 				slidesPerView: 3,
 			},
 		},
 		modules: [Navigation, Pagination],
 	});
 } catch (e) {}
+// Swiper
 
-
+// Tabs
 try {
-	const tabs = document.querySelectorAll(".catalog__btn");
+  const tabs = document.querySelectorAll(".catalog__btn");
 	const contents = document.querySelectorAll(".catalog__content-item");
 
 	tabs.forEach((tab, index) => {
@@ -57,13 +61,91 @@ try {
 			// Удаляем активный класс у всех табов и контента
 			tabs.forEach((t) => t.classList.remove("catalog__btn_active"));
 			contents.forEach((c) => (c.style.display = "none"));
-
+      
 			// Добавляем активный класс к нажатому табу и показываем соответствующий контент
 			tab.classList.add("catalog__btn_active");
 			contents[index].style.display = "flex";
 		});
 	});
-
+  
 	// Показываем первый контент при загрузке
 	contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
 } catch (e) {}
+// Tabs
+
+// Form
+try {
+  const validator = new JustValidate('#form', {submitFormAutomatically: true});
+
+  validator
+  .addField('#name', [
+      {
+        rule: 'required',
+        errorMessage: 'Пожалуйста введите ваше имя'
+      },
+      {
+        rule: 'minLength',
+        value: 2,
+        errorMessage: 'Минимальная длина 2 символа'
+      }
+    ])
+    .addField('#email', [
+      {
+        rule: 'required',
+        errorMessage: 'Пожалуйста введите ваш email'
+      },
+      {
+        rule: 'email',
+        errorMessage: 'Пожалуйста введите ваш email правильно'
+      }
+    ])
+    .addField('#question', [
+      {
+        rule: 'required',
+        errorMessage: 'Пожалуйста введите ваше сообщение'
+      },
+      {
+        rule: 'minLength',
+        value: 5,
+        errorMessage: 'Минимальная длина 5 символов'
+      }
+    ])
+    .addField('#checkbox', [
+      {
+        rule: 'required',
+      }
+    ], {
+      errorsContainer: document.querySelector('#checkbox')
+      .parentElement.parentElement.querySelector('.checkbox-error-message')
+    });
+} catch (e) {}
+// Form
+
+// Form2
+try {
+    const validatorForm = new JustValidate('#form2');
+
+  validatorForm
+  .addField('#email2', [
+      {
+        rule: 'required',
+        errorMessage: 'Пожалуйста введите ваш email'
+      },
+      {
+        rule: 'email',
+        errorMessage: 'Пожалуйста введите ваш email правильно'
+      }
+    ], {
+      errorsContainer: document.querySelector('#email2')
+      .parentElement.querySelector('.error-message')
+    })
+    .addField('#checkbox2', [
+      {
+        rule: 'required',
+      }
+    ], {
+      errorsContainer: document.querySelector('#checkbox')
+      .parentElement.querySelector('.checkbox-error-message')
+    });
+} catch (e) {}
+// Form2
